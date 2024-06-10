dfcae0311e2d23281b2ae458f123d293628c7795
Vaishnavy Prakash <vaishnavy.prakas@tigeranalytics.local>
2024-06-10 21:19:33 +0530

    updated code

---
+++ src/main/java/com/example/HelloWorldFunction.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+public class HelloWorldFunction {
+    @FunctionName("hello")
+    public HttpResponseMessage run(
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        return request.createResponseBuilder(HttpStatus.OK).body("Hello, World!").build();
+    }
+}
+
