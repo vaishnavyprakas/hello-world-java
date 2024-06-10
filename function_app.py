4ceaa897b6e87d3a1ecb8b2b05cb7748b322f7f5
Vaishnavy Prakash <vaishnavy.prakas@tigeranalytics.local>
2024-06-10 22:54:17 +0530

    updated code

--- function_app.py
+++ function_app.py
@@ -1,36 +1,12 @@
-# import azure.functions as func
-# import logging
-
-# app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-# @app.route(route="function_app_helloworld")
-# def function_app_helloworld(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-
-#     name = req.params.get('name')
-#     if not name:
-#         try:
-#             req_body = req.get_json()
-#         except ValueError:
-#             pass
-#         else:
-#             name = req_body.get('name')
-
-#     if name:
-#         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-#     else:
-#         return func.HttpResponse(
-#              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Koooiiiii",
-#              status_code=200
-#         )
-
-
-
-import logging
 import azure.functions as func
+import logging
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="function_app_helloworlds")
+def function_app_helloworld(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
     name = req.params.get('name')
     if not name:
         try:
@@ -39,10 +15,34 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+
     if name:
-        return func.HttpResponse(f"Hello, {name}!")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Koooiiiii",
+             status_code=200
         )
+
+
+
+# import logging
+# import azure.functions as func
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
+#     if name:
+#         return func.HttpResponse(f"Hello, {name}!")
+#     else:
+#         return func.HttpResponse(
+#              "Please pass a name on the query string or in the request body",
+#              status_code=400
+#         )
