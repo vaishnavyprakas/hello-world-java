45362651c00019ddb2fc089ab845c345ac993748
Vaishnavy Prakash <vaishnavy.prakas@tigeranalytics.local>
2024-06-10 22:42:27 +0530

    updated code

--- function_app.py
+++ function_app.py
@@ -17,9 +17,9 @@ def function_app_helloworld(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Koooiiii")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Koooiiiii",
              status_code=200
         )
